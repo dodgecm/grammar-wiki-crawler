a4e250e43833676f85f3ecb6cfb35ff2279dff80
dodgecm <tuamater@gmail.com>
2017-06-05 18:17:33 -0400

    Let's not ping their server ten billion times.

--- index_crawler.js
+++ index_crawler.js
@@ -5,6 +5,8 @@
 const request = require('request')
 const cheerio = require('cheerio')
 const URL = require('url-parse')
+const fs = require('fs')
+const crypto = require('crypto')
 
 const INDEX_URLS = [
   'https://resources.allsetlearning.com/chinese/grammar/A1_grammar_points',
@@ -19,30 +21,45 @@ const DEBUG_URL = 'https://resources.allsetlearning.com/chinese/grammar/A1_gramm
 const pagesToVisit = []
 let baseUrl
 
-// crawlIndexes(INDEX_URLS)
-crawlIndexes([DEBUG_URL])
+crawlIndexes(INDEX_URLS)
+// crawlIndexes([DEBUG_URL])
 
 function crawlIndexes(indexes) {
   for (let i = 0; i < indexes.length; i++) {
     const index = indexes[i]
     const url = new URL(index)
     baseUrl = `${url.protocol}//${url.hostname}`
-    visitPage(index)
+    loadPage(index, parsePage)
   }
 }
 
-function visitPage(url) {
-  // Make the request
-  console.log('Visiting page ', url)
-  request(url, (error, response, body) => {
-    // Check status code (200 is HTTP OK)
-    console.log('Status code: ', response.statusCode)
-    if (response.statusCode !== 200) { return }
+function loadPage(index, callback) {
+  const indexHash = crypto.createHash('md5').update(index).digest('hex')
+  const cachePath = `cache/${indexHash}.html`
 
-    // Parse the document body
-    const $ = cheerio.load(body)
-    collectInternalLinks($)
+  fs.readFile(cachePath, (readError, data) => {
+    if (!readError) { callback(data) }
+    else {
+      request(index, (error, response, body) => {
+        // Check status code (200 is HTTP OK)
+        console.log(`Loaded ${index} with code:`, response.statusCode)
+        if (response.statusCode !== 200) { throw error }
+
+        fs.writeFile(cachePath, body, writeError => {
+          if (writeError) { throw writeError }
+          console.log(`Wrote ${cachePath} to cache.`)
+        })
+
+        callback(body)
+      })
+    }
   })
+}
+
+function parsePage(body) {
+  // Parse the document body
+  const $ = cheerio.load(body)
+  collectInternalLinks($)
 }
 
 function collectInternalLinks($) {
